2e5368739ccdd1e203d0462f417fdc200a664cf7
Subin Cho <109792191+chsubinn@users.noreply.github.com>
2022-10-27 14:08:38 +0900

    Update 407.py

--- 407.py
+++ 407.py
@@ -20,5 +20,21 @@ def solution(s):
         answer=''
     return min
   
-  # 모범 답안
+# 모범 답안
+def solution(s):
+    answer = len(s)
+    for step in range (1, len(s)//2+1):
+        compressed=""
+        prev = s[0:step]
+        count=1
+        for j in range (step, len(s), step): # step만큼 증가시키며 이전 문자열과 비교
+            if prev == s[j:j+step]:
+                count+=1
+            else:
+                compressed += str(count)+prev if count>=2 else prev
+                prev = s[j:j+step]
+                count=1
+        compressed += str(count)+prev if count>=2 else prev
+        answer = min(answer, len(compressed))
+    return
   
